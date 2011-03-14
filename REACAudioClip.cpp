955c6bfc3aa78c10c95ebdf062178373a4d6b78c
Per Eckerdal <per.eckerdal@gmail.com>
2011-03-14 09:43:57 +0100

    Added better logging of audio drop outs

--- REACAudioClip.cpp
+++ REACAudioClip.cpp
@@ -26,27 +26,27 @@
 //		numSampleFrames - the total number of sample frames to clip and convert
 //		streamFormat - the current format of the IOAudioStream this function is operating on
 //		audioStream - the audio stream this function is operating on
-IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* outTargetBuffer, UInt32 inFirstFrame, UInt32 inNumberFrames, const IOAudioStreamFormat* inFormat, IOAudioStream* /*inStream*/)
+IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* destBuf, UInt32 firstSampleFrame, UInt32 numSampleFrames, const IOAudioStreamFormat* streamFormat, IOAudioStream* /*audioStream*/)
 {
 	//	figure out what sort of blit we need to do
-	if((inFormat->fSampleFormat == kIOAudioStreamSampleFormatLinearPCM) && inFormat->fIsMixable)
+	if((streamFormat->fSampleFormat == kIOAudioStreamSampleFormatLinearPCM) && streamFormat->fIsMixable)
 	{
 		//	it's mixable linear PCM, which means we will be calling a blitter, which works in samples not frames
 		Float32* theMixBuffer = (Float32*)inMixBuffer;
-		UInt32 theFirstSample = inFirstFrame * inFormat->fNumChannels;
-		UInt32 theNumberSamples = inNumberFrames * inFormat->fNumChannels;
+		UInt32 theFirstSample = firstSampleFrame * streamFormat->fNumChannels;
+		UInt32 theNumberSamples = numSampleFrames * streamFormat->fNumChannels;
         
-		if(inFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationSignedInt)
+		if(streamFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationSignedInt)
 		{
 			//	it's some kind of signed integer, which we handle as some kind of even byte length
 			bool nativeEndianInts;
 #if TARGET_RT_BIG_ENDIAN
-            nativeEndianInts = (inFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian);
+            nativeEndianInts = (streamFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian);
 #else
-            nativeEndianInts = (inFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian);
+            nativeEndianInts = (streamFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian);
 #endif
 			
-			switch(inFormat->fBitWidth)
+			switch(streamFormat->fBitWidth)
 			{
 				case 8:
                 {
@@ -57,7 +57,7 @@ IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* outTa
                     
 				case 16:
                 {
-                    SInt16* theTargetBuffer = (SInt16*)outTargetBuffer;
+                    SInt16* theTargetBuffer = (SInt16*)destBuf;
                     if (nativeEndianInts)
                         Float32ToNativeInt16(&(theMixBuffer[theFirstSample]), &(theTargetBuffer[theFirstSample]), theNumberSamples);
                     else
@@ -67,7 +67,7 @@ IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* outTa
                     
 				case 24:
                 {
-                    UInt8* theTargetBuffer = (UInt8*)outTargetBuffer;
+                    UInt8* theTargetBuffer = (UInt8*)destBuf;
                     if (nativeEndianInts)
                         Float32ToNativeInt24(&(theMixBuffer[theFirstSample]), &(theTargetBuffer[3*theFirstSample]), theNumberSamples);
                     else
@@ -77,7 +77,7 @@ IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* outTa
                     
 				case 32:
                 {
-                    SInt32* theTargetBuffer = (SInt32*)outTargetBuffer;
+                    SInt32* theTargetBuffer = (SInt32*)destBuf;
                     if (nativeEndianInts)
                         Float32ToNativeInt32(&(theMixBuffer[theFirstSample]), &(theTargetBuffer[theFirstSample]), theNumberSamples);
                     else
@@ -87,29 +87,29 @@ IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* outTa
                     
 				default:
 					IOLog("REACAudioEngine::clipOutputSamples(): Can't handle signed integers "\
-                          "with a bit width of %d.\n", inFormat->fBitWidth);
+                          "with a bit width of %d.\n", streamFormat->fBitWidth);
 					break;
                     
 			}
 		}
-		else if(inFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationIEEE754Float)
+		else if(streamFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationIEEE754Float)
 		{
 			//	it is some kind of floating point format
 #if TARGET_RT_BIG_ENDIAN
-			if((inFormat->fBitWidth == 32) && (inFormat->fBitDepth == 32) && (inFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian))
+			if((streamFormat->fBitWidth == 32) && (streamFormat->fBitDepth == 32) && (streamFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian))
 #else
-                if((inFormat->fBitWidth == 32) && (inFormat->fBitDepth == 32) && (inFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian))
+                if((streamFormat->fBitWidth == 32) && (streamFormat->fBitDepth == 32) && (streamFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian))
 #endif
                 {
                     //	it's Float32, so we are just going to copy the data
-                    Float32* theTargetBuffer = (Float32*)outTargetBuffer;
+                    Float32* theTargetBuffer = (Float32*)destBuf;
                     memcpy(&(theTargetBuffer[theFirstSample]), &(theMixBuffer[theFirstSample]), theNumberSamples * sizeof(Float32));
                 }
                 else
                 {
                     IOLog("REACAudioEngine::clipOutputSamples(): Can't handle floats with a bit width "\
-                          "of %d, bit depth of %d, and/or the given byte order.\n", inFormat->fBitWidth,
-                          inFormat->fBitDepth);
+                          "of %d, bit depth of %d, and/or the given byte order.\n", streamFormat->fBitWidth,
+                          streamFormat->fBitDepth);
                 }
 		}
 	}
@@ -117,9 +117,9 @@ IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* outTa
 	{
 		//	it's not linear PCM or it's not mixable, so just copy the data into the target buffer
 		SInt8* theMixBuffer = (SInt8*)inMixBuffer;
-		SInt8* theTargetBuffer = (SInt8*)outTargetBuffer;
-		UInt32 theFirstByte = inFirstFrame * (inFormat->fBitWidth / 8) * inFormat->fNumChannels;
-		UInt32 theNumberBytes = inNumberFrames * (inFormat->fBitWidth / 8) * inFormat->fNumChannels;
+		SInt8* theTargetBuffer = (SInt8*)destBuf;
+		UInt32 theFirstByte = firstSampleFrame * (streamFormat->fBitWidth / 8) * streamFormat->fNumChannels;
+		UInt32 theNumberBytes = numSampleFrames * (streamFormat->fBitWidth / 8) * streamFormat->fNumChannels;
 		memcpy(&(theTargetBuffer[theFirstByte]), &(theMixBuffer[theFirstByte]), theNumberBytes);
 	}
     
@@ -141,28 +141,46 @@ IOReturn REACAudioEngine::clipOutputSamples(const void* inMixBuffer, void* outTa
 //		numSampleFrames - the total number of sample frames to convert and scale
 //		streamFormat - the current format of the IOAudioStream this function is operating on
 //		audioStream - the audio stream this function is operating on
-IOReturn REACAudioEngine::convertInputSamples(const void* inSourceBuffer, void* outTargetBuffer, UInt32 inFirstFrame,
-                                              UInt32 inNumberFrames, const IOAudioStreamFormat* inFormat,
-                                              IOAudioStream* /*inStream*/) {
+IOReturn REACAudioEngine::convertInputSamples(const void* sampleBuf, void* destBuf, UInt32 firstSampleFrame,
+                                              UInt32 numSampleFrames, const IOAudioStreamFormat* streamFormat,
+                                              IOAudioStream* /*audioStream*/) {
+    { // Check if we'll have an audio drop out, and log if that's the case.
+        const int numChannels = inputStream->format.fNumChannels;
+        const int resolution = inputStream->format.fBitWidth/8;
+        const int bytesPerSample = resolution * numChannels;
+        
+        // This is the place in the buffer where we're currently receiving data from the network
+        UInt8 *inBufferPosition = (UInt8 *)mInBuffer + currentBlock*blockSize*bytesPerSample;
+        // Pointers to where we'll begin and stop writing
+        UInt8 *bufferBeginWritePosition = ((UInt8 *)sampleBuf) + firstSampleFrame*bytesPerSample;
+        UInt8 *bufferStopWritePosition = bufferBeginWritePosition + numSampleFrames*bytesPerSample;
+        
+        // Check if we're going to cross inBufferPosition (this leads to audio dropouts)
+        if (inBufferPosition >= bufferBeginWritePosition && inBufferPosition < bufferStopWritePosition) {
+            IOLog("REACAudioEngine::convertInputSamples(): Audio drop-out! (by %d samples, when converting %d samples)\n",
+                  (int) (firstSampleFrame+numSampleFrames - currentBlock*blockSize), (int) numSampleFrames);
+        }
+    }
+    
 	//	figure out what sort of blit we need to do
-	if((inFormat->fSampleFormat == kIOAudioStreamSampleFormatLinearPCM) && inFormat->fIsMixable)
+	if((streamFormat->fSampleFormat == kIOAudioStreamSampleFormatLinearPCM) && streamFormat->fIsMixable)
 	{
 		//	it's linear PCM, which means the target is Float32 and we will be calling a blitter, which works in samples not frames
-		Float32* theTargetBuffer = (Float32*)outTargetBuffer;
-		UInt32 theFirstSample = inFirstFrame * inFormat->fNumChannels;
-		UInt32 theNumberSamples = inNumberFrames * inFormat->fNumChannels;
+		Float32* theTargetBuffer = (Float32*)destBuf;
+        const UInt32 theFirstSample = firstSampleFrame * inputStream->format.fNumChannels;
+        const UInt32 theNumberSamples = numSampleFrames * inputStream->format.fNumChannels;
         
-		if(inFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationSignedInt)
+		if(streamFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationSignedInt)
 		{
 			//	it's some kind of signed integer, which we handle as some kind of even byte length
 			bool nativeEndianInts;
 #if TARGET_RT_BIG_ENDIAN
-            nativeEndianInts = (inFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian);
+            nativeEndianInts = (streamFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian);
 #else
-            nativeEndianInts = (inFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian);
+            nativeEndianInts = (streamFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian);
 #endif
 			
-			switch(inFormat->fBitWidth)
+			switch(streamFormat->fBitWidth)
 			{
 				case 8:
                 {
@@ -173,7 +191,7 @@ IOReturn REACAudioEngine::convertInputSamples(const void* inSourceBuffer, void* 
                     
 				case 16:
                 {
-                    SInt16* theSourceBuffer = (SInt16*)inSourceBuffer;
+                    SInt16* theSourceBuffer = (SInt16*)sampleBuf;
                     if (nativeEndianInts)
                         NativeInt16ToFloat32(&(theSourceBuffer[theFirstSample]), theTargetBuffer, theNumberSamples);
                     else
@@ -183,7 +201,7 @@ IOReturn REACAudioEngine::convertInputSamples(const void* inSourceBuffer, void* 
                     
 				case 24:
                 {
-                    UInt8* theSourceBuffer = (UInt8*)inSourceBuffer;
+                    UInt8* theSourceBuffer = (UInt8*)sampleBuf;
                     if (nativeEndianInts)
                         NativeInt24ToFloat32(&(theSourceBuffer[3*theFirstSample]), theTargetBuffer, theNumberSamples);
                     else
@@ -193,7 +211,7 @@ IOReturn REACAudioEngine::convertInputSamples(const void* inSourceBuffer, void* 
                     
 				case 32:
                 {
-                    SInt32* theSourceBuffer = (SInt32*)inSourceBuffer;
+                    SInt32* theSourceBuffer = (SInt32*)sampleBuf;
                     if (nativeEndianInts)
                         NativeInt32ToFloat32(&(theSourceBuffer[theFirstSample]), theTargetBuffer, theNumberSamples);
                     else
@@ -203,95 +221,40 @@ IOReturn REACAudioEngine::convertInputSamples(const void* inSourceBuffer, void* 
                     
 				default:
 					IOLog("REACAudioEngine::convertInputSamples(): can't handle signed integers with a bit width of %d.\n",
-                          inFormat->fBitWidth);
+                          streamFormat->fBitWidth);
 					break;
                     
 			}
 		}
-		else if(inFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationIEEE754Float)
+		else if(streamFormat->fNumericRepresentation == kIOAudioStreamNumericRepresentationIEEE754Float)
 		{
 			//	it is some kind of floating point format
 #if TARGET_RT_BIG_ENDIAN
-			if((inFormat->fBitWidth == 32) && (inFormat->fBitDepth == 32) && (inFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian))
+			if((streamFormat->fBitWidth == 32) && (streamFormat->fBitDepth == 32) && (streamFormat->fByteOrder == kIOAudioStreamByteOrderBigEndian))
 #else
-                if((inFormat->fBitWidth == 32) && (inFormat->fBitDepth == 32) && (inFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian))
+                if((streamFormat->fBitWidth == 32) && (streamFormat->fBitDepth == 32) && (streamFormat->fByteOrder == kIOAudioStreamByteOrderLittleEndian))
 #endif
                 {
                     //	it's Float32, so we are just going to copy the data
-                    Float32* theSourceBuffer = (Float32*)inSourceBuffer;
+                    Float32* theSourceBuffer = (Float32*)sampleBuf;
                     memcpy(theTargetBuffer, &(theSourceBuffer[theFirstSample]), theNumberSamples * sizeof(Float32));
                 }
                 else
                 {
                     IOLog("REACEngine::convertInputSamples(): can't handle floats with a bit width of %d, "\
                           "bit depth of %d, and/or the given byte order.\n",
-                          inFormat->fBitWidth, inFormat->fBitDepth);
+                          streamFormat->fBitWidth, streamFormat->fBitDepth);
                 }
 		}
 	}
 	else
 	{
 		//	it's not linear PCM or it's not mixable, so just copy the data into the target buffer
-		SInt8* theSourceBuffer = (SInt8*)inSourceBuffer;
-		UInt32 theFirstByte = inFirstFrame * (inFormat->fBitWidth / 8) * inFormat->fNumChannels;
-		UInt32 theNumberBytes = inNumberFrames * (inFormat->fBitWidth / 8) * inFormat->fNumChannels;
-		memcpy(outTargetBuffer, &(theSourceBuffer[theFirstByte]), theNumberBytes);
+		SInt8* theSourceBuffer = (SInt8*)sampleBuf;
+		UInt32 theFirstByte = firstSampleFrame * (streamFormat->fBitWidth / 8) * streamFormat->fNumChannels;
+		UInt32 theNumberBytes = numSampleFrames     * (streamFormat->fBitWidth / 8) * streamFormat->fNumChannels;
+		memcpy(destBuf, &(theSourceBuffer[theFirstByte]), theNumberBytes);
 	}
     
 	return kIOReturnSuccess;
 }
-
-
-
-#if 0
-
-IOReturn REACAudioEngine::convertInputSamples(const void *sampleBuf, void *destBuf, UInt32 firstSampleFrame,
-                                              UInt32 numSampleFrames, const IOAudioStreamFormat *streamFormat,
-                                              IOAudioStream *audioStream) {
-    // IOLog("REACAudioEngine::convertInputSamples()\n");
-    
-    UInt32 numChannels = streamFormat->fNumChannels;
-    int resolution = streamFormat->fBitWidth/8;
-    float *floatDestBuf = (float *)destBuf;
-    
-    UInt32 numSamplesLeft = numSampleFrames * numChannels;
-    UInt8 *inputBuf = &(((UInt8 *)sampleBuf)[firstSampleFrame * numChannels * resolution]);
-    
-    bool haveComplained = false; // TODO Debug
-    
-    // Loop through each sample and scale and convert them
-    while (numSamplesLeft > 0) {
-        SInt32 inputSample;
-        
-        if (55 == inputBuf[0] && 55 == inputBuf[1] && 55 == inputBuf[2] && !haveComplained) {
-            IOLog("REACAudioEngine::convertInputSamples(): CLIP? (%d, %d)\n", (int) firstSampleFrame, (int) numSampleFrames);
-            haveComplained = true;
-        }
-        
-        // Fetch the 24 bit input sample
-        inputSample = inputBuf[2];
-        inputSample = (inputSample << 8) | inputBuf[1];
-        inputSample = (inputSample << 8) | inputBuf[0];
-        
-        // Fill the rest with ones if the number is negative (this is required as inputSample is SInt32)
-        if (inputSample & 0x800000)
-            inputSample |= ~0xffffff;
-        
-        const float Q = 1.0f / (0x7fffff + 0.5f);
-        
-        *floatDestBuf = (inputSample + 0.5f) * Q;
-        
-        inputBuf[0] = 55; // TODO Debug
-        inputBuf[1] = 55; // TODO Debug
-        inputBuf[2] = 55; // TODO Debug
-        
-        // Move on to the next sample
-        inputBuf += resolution;
-        ++floatDestBuf;
-        --numSamplesLeft;
-    }
-    
-    return kIOReturnSuccess;
-}
-
-#endif
