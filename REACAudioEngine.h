753367072de89397986fb2a497d370a811fb1060
Per Eckerdal <per.eckerdal@gmail.com>
2011-07-04 16:25:45 +0200

    Licensed the code (except code based on Apple's PCM blitter lib) under the GPL

--- REACAudioEngine.h
+++ REACAudioEngine.h
@@ -1,6 +1,27 @@
 /*
-  File:REACAudioEngine.h
-*/
+ *  REACAudioEngine.h
+ *  REAC
+ *
+ *  Copyright 2011 Per Eckerdal. All rights reserved.
+ *  
+ *  
+ *  This file is part of the OS X REAC driver.
+ *  
+ *  The OS X REAC driver is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *  
+ *  The OS X REAC driver is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with OS X REAC driver.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef _REACAUDIOENGINE_H
 #define _REACAUDIOENGINE_H
--- REACConnection.h
+++ REACConnection.h
@@ -4,6 +4,23 @@
  *
  *  Created by Per Eckerdal on 02/01/2011.
  *  Copyright 2011 Per Eckerdal. All rights reserved.
+ *  
+ *  
+ *  This file is part of the OS X REAC driver.
+ *  
+ *  The OS X REAC driver is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *  
+ *  The OS X REAC driver is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with OS X REAC driver.  If not, see
+ *  <http://www.gnu.org/licenses/>.
  *
  */
 
